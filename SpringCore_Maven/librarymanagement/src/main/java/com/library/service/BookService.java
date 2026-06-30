2bc28e96e994d40b2aa0a86d7db65acf6933d4e9
poojasri-elango47 <poojasrielango47@gmail.com>
2026-06-30 11:43:23 +0530

    Spring Core Ex1

---
+++ SpringCore_Maven/librarymanagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,16 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+public class BookService {
+
+   private  BookRepository bookRepository;
+    public void setBookRepository(BookRepository bookRepository){
+        this.bookRepository=bookRepository;
+    }
+
+    public void DisplayBookService(){
+        System.out.println("This is Book Service");
+        bookRepository.DisplayBookRepo();
+    }
+    
+}
