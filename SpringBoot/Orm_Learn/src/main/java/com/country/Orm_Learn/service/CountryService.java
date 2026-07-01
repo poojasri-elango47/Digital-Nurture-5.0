37a16431b3b45bb1e36422deebb50f6373df6fd7
poojasri-elango47 <poojasrielango47@gmail.com>
2026-07-01 16:21:31 +0530

    Spring data jpa handson

---
+++ SpringBoot/Orm_Learn/src/main/java/com/country/Orm_Learn/service/CountryService.java
@@ -0,0 +1,22 @@
+package com.country.Orm_Learn.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.country.Orm_Learn.model.Country;
+import com.country.Orm_Learn.repository.CountryRepository;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
